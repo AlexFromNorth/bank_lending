00c83da6a415253b749ce1af828b10cfa0755df9
AlexFromNorth_desc <jacklex.ab@gmail.com>
2024-09-12 20:55:07 +0300

    fixed animation bugs

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerButton = document.getElementById('burger-button');
     const menuList = document.getElementById('menu-list');
+    const mainSection = document.querySelector('.main');
 
     burgerButton.addEventListener('click', () => {
         menuList.classList.toggle('show');
+        mainSection.classList.toggle('shadow')
     });
 });
 
@@ -28,4 +30,42 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
   
+  
+
+// burger menu
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.click-menu div');
+    const clickMenu = document.querySelector('.click-menu a');
+    const menuTriangle = document.querySelector('.click-menu .triangle-icon');
+  
+    // Открытие/закрытие меню при клике на основное меню
+    clickMenu.addEventListener('click', (event) => {
+      event.stopPropagation(); // Останавливаем всплытие события, чтобы клик на меню не закрывал его
+      menu.classList.toggle('active');
+      console.log(menuTriangle)
+      menuTriangle.classList.toggle('active');
+    });
+  
+
+
+    const offer = document.querySelector('.click-offer div');
+    const clickOffer = document.querySelector('.click-offer a');
+    const offetTriangle = document.querySelector('.click-offer .triangle-icon');
+
+  
+    // Открытие/закрытие меню при клике на основное меню
+    clickOffer.addEventListener('click', (event) => {
+        event.stopPropagation(); // Останавливаем всплытие события, чтобы клик на меню не закрывал его
+        offer.classList.toggle('active');
+        offetTriangle.classList.toggle('active');
+      });
+  
+
+  });
+
+  
+  
+
